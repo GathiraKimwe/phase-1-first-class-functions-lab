ed5da7f07151f10ff1fb0eb0b9877ba876c49d13
Edwin Gathira <gathirakimwe@gmail.com>
2022-08-21 12:28:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arg) => {
+    return arg.slice(0,2);
+  }
+  
+  const returnLastTwoDrivers = (arg) => {
+    return arg.slice(-2);
+  }
+  
+  let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  function createFareMultiplier(arg) {
+    return function fareMultiplier() {
+      return arg * arg;
+    }
+  }
+  
+  const fareDoubler = (arg) => {
+    return arg * 2;
+  }
+  
+  const fareTripler = (arg) => {
+    return arg * 3;
+  }
+  
+  const selectDifferentDrivers = (arg1, arg2) => {
+    return arg2(arg1);
+  }
